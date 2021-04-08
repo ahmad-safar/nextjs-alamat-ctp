f8cae9de20dbc2142c337f691ae58a53469d5078
Ahmad Safar <ahmadsafar88@gmail.com>
2021-04-08 20:52:53 +0800

    chore: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,4 +7,18 @@ module.exports = {
     },
     // optionally a second project
   ],
+  deploy: {
+    production: {
+      user: 'safar',
+      host: 'tselpamasuka.com',
+      ref: 'origin/master',
+      repo: 'https://github.com/faropedia/nextjs-alamat-ctp',
+      path: '/home/safar/tselpamasuka.com/alamat-ctp',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'npm install && npm run build && pm2 reload ecosystem.config.js',
+      'post-setup':
+        'cp .env.local.example .env.local',
+    },
+  },
 }
