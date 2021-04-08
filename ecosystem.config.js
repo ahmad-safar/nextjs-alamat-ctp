c45bcc4dbd6d000f92f8f1a08ff09f78495a10c3
Ahmad Safar <ahmadsafar88@gmail.com>
2021-04-08 20:55:38 +0800

    chore: fix ref

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     production: {
       user: 'safar',
       host: 'tselpamasuka.com',
-      ref: 'origin/master',
+      ref: 'origin/main',
       repo: 'https://github.com/faropedia/nextjs-alamat-ctp',
       path: '/home/safar/tselpamasuka.com/alamat-ctp',
       'pre-deploy-local': '',
